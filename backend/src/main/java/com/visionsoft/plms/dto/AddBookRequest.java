37b29c9d5653bb3c94de9540d006e41a60e13538
ozlemmalkoc <ozlem_malkoc19@hotmail.com>
2025-12-30 23:57:06 +0300

    .

---
+++ backend/src/main/java/com/visionsoft/plms/dto/AddBookRequest.java
@@ -0,0 +1,21 @@
+package com.visionsoft.plms.dto;
+
+import lombok.Data; // Lombok kütüphanesini çağırıyoruz
+
+@Data // Bu sihirli kelime, arka planda tüm getter/setter'ları yaratır
+public class AddBookRequest {
+    // LibraryItem (Ortak)
+    private String title;
+    private String description;
+
+    // --- YENİ EKLENEN KISIM ---
+    private Boolean favorite; // True gelirse favorilere ekle
+
+    // Book (Özel)
+    private String isbn;
+    private String author;
+    private String publisher;
+    private Integer publicationYear;
+    private Integer pageCount;
+    private String genre;
+}
